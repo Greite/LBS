ae8ea97029bed3ac0c39c76feb311f2140433b28
leobob70 <leo.bernier70@hotmail.fr>
2017-12-05 16:51:12 +0100

    recup

--- src/model/Categorie.php
+++ src/model/Categorie.php
@@ -2,11 +2,11 @@
 /**
 * 
 */
-
 namespace lbs\model;
 
 class Categorie extends \Illuminate\Database\Eloquent\Model
 {
 	protected $table = 'categorie';
 	protected $primaryKey = 'id';
+	public $timestamp = false;
 }
