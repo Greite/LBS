3753dd81b286f4ba6544562bf0005885cdcb5545
Gauthier <core.editor>
2017-12-05 16:14:13 +0100

    TAGROSSE

--- src/control/LbsController.php
+++ src/control/LbsController.php
@@ -1,6 +1,7 @@
 <?php 
 namespace lbs\control ;
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 use \lbs\model\Categorie as Categorie;
@@ -12,18 +13,17 @@ class LbsController{
         $resp = $resp->withHeader('Content-Type', "application/json;charset=utf-8");
         $resp->getBody()->write(json_encode($tablal->toArray()));
         return $resp;
-        }
+    }
     
     public function categoriesId(Request $req, Response $resp, $args) {
-        try {
+        try{
             $cats = Categorie::where("id", "=", $args['id'])->firstOrFail();
-        } catch (Exception $e) {
+        }catch(ModelNotFoundException $e){
             $resp = $resp->withStatus(404);
             $resp = $resp->withJson(array('type' => 'error', 'error' => 404, 'message' => 'Ressource non disponible : /categorie/'.$args['id']));
             return $resp;
         }
         $resp = $resp->withJson($cats);
         return $resp;
-            
-        }
+    }
 }
