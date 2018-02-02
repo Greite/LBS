81b9cb52a5417cb447f23f77f76554eb11bc1fc9
Maxime <maximesd52@gmail.com>
2018-02-02 20:13:00 +0100

    ajout sql

---
+++ sql/sandwichs.sql
@@ -0,0 +1,7 @@
+INSERT INTO `sandwich` (`id`, `nom`, `description`, `type_pain`, `img`) VALUES
+(4,	'bucheron',	'sandwich de la forêt',	'arbre',	NULL),
+(5,	'jambon-beurre',	'le jambon-beurre traditionnel, avec des cornichons',	'baguette',	NULL),
+(6,	'fajitas poulet',	'fajitas au poulet avec ses tortillas de mais, comme à Puebla',	'tortillas',	NULL),
+(7,	'le forestier',	'un bon sandwich au gout de la forêt',	'pain complet',	NULL),
+(118,	'le renard',	'son pere le renard',	'pain',	NULL),
+(119,	'le mille-feuilles',	'mille belles feuilles de chêne dans du pain',	'baguette',	NULL);
